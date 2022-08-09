0133e2779405dff0b13989e56d95d38c6c5efcad
VPagent <vpagent13@gmail.com>
2022-08-09 18:18:13 +0300

    videodone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryContainer = document.querySelector(".gallery")
+
+let allPictures = ""
+
+galleryItems.forEach(obj  => {
+    allPictures += `<li><a class="gallery__item" href="${obj.original}">
+    <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+  </a></li>`
+})
+galleryContainer.innerHTML= allPictures
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player'
+import  throttle  from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
 
+    player.on('timeupdate', throttle(getCurrentTime, 1000)) 
+    function getCurrentTime({seconds}){
+        localStorage.setItem("videoplayer-current-time", seconds)
+    }
+    if(localStorage.getItem("videoplayer-current-time")){
+        let currentSec = localStorage.getItem("videoplayer-current-time")
+        player.setCurrentTime(currentSec)
+    }
+
+
+    
+     
+    
+    
