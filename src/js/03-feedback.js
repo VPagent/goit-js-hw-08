433730a248a60470ade2b5d2ca627da2aa638ed2
VPagent <vpagent13@gmail.com>
2022-08-09 19:49:52 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+import  throttle  from 'lodash.throttle';
+const form = document.querySelector(".feedback-form")
+
+
+
+window.addEventListener("load", onLoaded)
+
+const obj = {}
+
+function onLoaded(event){
+    if(localStorage.getItem("feedback-form-state")){
+        const newObj = JSON.parse(localStorage.getItem("feedback-form-state"))
+        form[0].value = newObj.email
+        form[1].value = newObj.message
+    }    
+}
+
+form.addEventListener("input", throttle(valueForm, 500))
+
+function valueForm (event) {
+    event.preventDefault()
+    obj[event.target.name] = event.target.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj))
+}
+
+form.addEventListener("submit", onSubmForm)
+
+function onSubmForm(event){
+    event.preventDefault()
+    console.log(obj)
+    form.reset()
+}
+
+
+
